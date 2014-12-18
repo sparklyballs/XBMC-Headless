baa153fc77a25d357faaaed153ed664df8d4a039
sparklyballs <sparklyballs@gmail.com>
2014-12-18 12:19:18 +0000

    Update firstrun.sh

--- src/firstrun.sh
+++ src/firstrun.sh
@@ -4,7 +4,8 @@ if [ -d "xbmcfiles /root/.xbmc/userdata" ]; then
 echo "using existing datafiles"
 else
 echo "creating datafiles"
-mv  /xbmcfiles/* /root/.xbmc/
+cp -pr  /xbmcfiles/* /root/.xbmc/
+sleep 30
 fi
 if [ -f "/root/.xbmc/userdata/advancedsettings.xml" ]; then
 echo "using existing advancedsettings.xml"
@@ -13,7 +14,7 @@ sed -i "s|\(<port>\)[^<>]*\(</port>\)|\1${MYSQLport}\2|" /root/.xbmc/userdata/ad
 sed -i "s|\(<user>\)[^<>]*\(</user>\)|\1${MYSQLuser}\2|" /root/.xbmc/userdata/advancedsettings.xml
 sed -i "s|\(<pass>\)[^<>]*\(</pass>\)|\1${MYSQLpass}\2|" /root/.xbmc/userdata/advancedsettings.xml
 else
-mv  /advancestore/advancedsettings.xml /root/.xbmc/userdata/advancedsettings.xml
+cp  /advancestore/advancedsettings.xml /root/.xbmc/userdata/advancedsettings.xml
 sed -i "s|\(<host>\)[^<>]*\(</host>\)|\1${MYSQLip}\2|" /root/.xbmc/userdata/advancedsettings.xml
 sed -i "s|\(<port>\)[^<>]*\(</port>\)|\1${MYSQLport}\2|" /root/.xbmc/userdata/advancedsettings.xml
 sed -i "s|\(<user>\)[^<>]*\(</user>\)|\1${MYSQLuser}\2|" /root/.xbmc/userdata/advancedsettings.xml
